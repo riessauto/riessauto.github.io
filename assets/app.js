37a7b6a8c707cf74f9b9d21bc6558c276d365283
riessauto <rafiksemco@gmail.com>
2026-06-05 08:46:18 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,13 @@
-document.addEventListener('DOMContentLoaded',()=>{let b=document.querySelector('.menu'),l=document.querySelector('.links');if(b&&l)b.onclick=()=>l.classList.toggle('open')});
+document.addEventListener('DOMContentLoaded',()=>{let b=document.querySelector('.menu'),l=document.querySelector('.links');if(b&&l)b.onclick=()=>l.classList.toggle('open')});document.addEventListener('click', function(e) {
+  var link = e.target.closest('a[href^="tel:"]');
+  if (!link) return;
+
+  var phone = link.getAttribute('href');
+
+  if (phone !== 'tel:0525877378') {
+    return;
+  }
+
+  e.preventDefault();
+  return gtag_report_conversion(link.href);
+});
